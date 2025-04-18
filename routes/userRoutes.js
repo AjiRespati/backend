8a0df6a323fa2afe7b43555f1c7249d013d8342a
Aji Respati <aji@voltras.co.id>
2025-04-18 08:00:36 +0700

    implement delete user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {  getAllUsers } = require("../controllers/userController");
+const {  getAllUsers, deleteUser } = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.get("/users", authMiddleware, getAllUsers);
+router.delete("/:id", authMiddleware, deleteUser);
 
 module.exports = router;
