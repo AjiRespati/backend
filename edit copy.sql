a88331b9e18dc430ee7df94ec90b0f1ca08ec267
Aji Respati <aji@voltras.co.id>
2025-06-02 08:17:50 +0700

    hmm

--- edit copy.sql
+++ edit copy.sql
@@ -1,22 +1,7 @@
 
 UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 32400
-WHERE id = 'aae9458b-889e-4a36-aacb-2eb04151128c';
-
-
-UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 97200
-WHERE id = 'a44e0b7f-8cff-4b06-a0e9-fc997737932c';
-
-
-UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 37800
-WHERE id = '4c937c3e-858f-4cd2-956c-29436b28fae5';
-
-
-UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 40500
-WHERE id = '350b882a-806d-4215-b345-31eef6aa7fe1';
+SET "totalDistributorShare" = 000
+WHERE id = '';
 
 
 UPDATE "public"."Stocks"
@@ -45,37 +30,6 @@ WHERE id = '62aea92f-ecb5-4145-a970-d96afc48d028';
 
 
 UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 40500
-WHERE id = '22aad666-8c79-4e6f-8acc-66b23046fc57';
-
-
-UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 29160
-WHERE id = '3825fb43-0903-4a40-bc5e-df4cd3222e1a';
-
-
-UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 37800
-WHERE id = '37a71e25-25a1-4899-8ca0-1e3fac56ab5d';
-
-
-UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 57000
-WHERE id = '6f4408a5-6506-46ce-9390-0e2d6c90c117';
-
-
-UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 29160
-WHERE id = 'e349a926-8cba-44f9-a649-c1f2dc0eff78';
-
-
-UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 37800
-WHERE id = 'b05d31ae-39cf-44a4-a33e-8a195df53523';
-
---------------------------
-
-UPDATE "public"."Stocks"
 SET "totalDistributorShare" = 27000
 WHERE id = '8d84fdeb-4b28-4fdc-a36c-09a8023370cb';
 
@@ -101,31 +55,6 @@ WHERE id = '1810bc13-bc33-4b8d-b5f1-7fd2f21b0ea6';
 
 
 UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 29160
-WHERE id = 'fe0a304a-15b8-41b6-8edd-0320add27241';
-
-
-UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 37800
-WHERE id = '40c7d8dd-3a6e-4044-94da-d93e05ed82cd';
-
-
-UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 57000
-WHERE id = '3cb6e255-3ec6-4679-8e4e-e54059697711';
-
-
-UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 29160
-WHERE id = '95b4e966-f35f-4970-b6c0-37336844b7b8';
-
-
-UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 40500
-WHERE id = '5c6993d8-c6e2-481f-9f8f-c22778ec74cd';
-
-
-UPDATE "public"."Stocks"
 SET "totalDistributorShare" = 27000
 WHERE id = '168ccf30-31a2-4d02-b5f7-add49192e380';
 
@@ -141,13 +70,23 @@ WHERE id = 'ab64ace4-c577-4001-8ce7-a8372c1c733e';
 
 
 UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 32400
-WHERE id = '6a356c0b-7d82-4b88-a922-b5221f83f948';
+SET "totalDistributorShare" = 29160
+WHERE id = '3825fb43-0903-4a40-bc5e-df4cd3222e1a';
 
 
 UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 40500
-WHERE id = '56c686e2-044a-4735-a0c4-66b2a973b1ef';
+SET "totalDistributorShare" = 29160
+WHERE id = 'e349a926-8cba-44f9-a649-c1f2dc0eff78';
+
+
+UPDATE "public"."Stocks"
+SET "totalDistributorShare" = 29160
+WHERE id = 'fe0a304a-15b8-41b6-8edd-0320add27241';
+
+
+UPDATE "public"."Stocks"
+SET "totalDistributorShare" = 29160
+WHERE id = '95b4e966-f35f-4970-b6c0-37336844b7b8';
 
 
 UPDATE "public"."Stocks"
@@ -156,5 +95,65 @@ WHERE id = 'e24b2317-d2b9-4572-bed5-d85a16c066da';
 
 
 UPDATE "public"."Stocks"
-SET "totalDistributorShare" = 000
-WHERE id = '';
+SET "totalDistributorShare" = 32400
+WHERE id = 'aae9458b-889e-4a36-aacb-2eb04151128c';
+
+
+UPDATE "public"."Stocks"
+SET "totalDistributorShare" = 32400
+WHERE id = '6a356c0b-7d82-4b88-a922-b5221f83f948';
+
+
+UPDATE "public"."Stocks"
+SET "totalDistributorShare" = 37800
+WHERE id = '4c937c3e-858f-4cd2-956c-29436b28fae5';
+
+
+UPDATE "public"."Stocks"
+SET "totalDistributorShare" = 37800
+WHERE id = '37a71e25-25a1-4899-8ca0-1e3fac56ab5d';
+
+
+UPDATE "public"."Stocks"
+SET "totalDistributorShare" = 37800
+WHERE id = 'b05d31ae-39cf-44a4-a33e-8a195df53523';
+
+
+UPDATE "public"."Stocks"
+SET "totalDistributorShare" = 37800
+WHERE id = '40c7d8dd-3a6e-4044-94da-d93e05ed82cd';
+
+
+UPDATE "public"."Stocks"
+SET "totalDistributorShare" = 40500
+WHERE id = '350b882a-806d-4215-b345-31eef6aa7fe1';
+
+
+UPDATE "public"."Stocks"
+SET "totalDistributorShare" = 40500
+WHERE id = '22aad666-8c79-4e6f-8acc-66b23046fc57';
+
+
+UPDATE "public"."Stocks"
+SET "totalDistributorShare" = 40500
+WHERE id = '5c6993d8-c6e2-481f-9f8f-c22778ec74cd';
+
+
+UPDATE "public"."Stocks"
+SET "totalDistributorShare" = 40500
+WHERE id = '56c686e2-044a-4735-a0c4-66b2a973b1ef';
+
+
+UPDATE "public"."Stocks"
+SET "totalDistributorShare" = 57000
+WHERE id = '6f4408a5-6506-46ce-9390-0e2d6c90c117';
+
+
+UPDATE "public"."Stocks"
+SET "totalDistributorShare" = 57000
+WHERE id = '3cb6e255-3ec6-4679-8e4e-e54059697711';
+
+
+UPDATE "public"."Stocks"
+SET "totalDistributorShare" = 97200
+WHERE id = 'a44e0b7f-8cff-4b06-a0e9-fc997737932c';
