a16e35636b339094899f685312f6ccfd70275de5
Aji Respati <aji@voltras.co.id>
2025-06-19 06:54:10 +0700

    upload shop n refrigerator image

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,14 +7,14 @@ const {
     deleteProduct,
 } = require("../controllers/productController");
 const authMiddleware = require("../middleware/authMiddleware");
-const {upload, imageCompressor } = require("../middleware/uploadMiddleware");
+const { upload, imageCompressor } = require("../middleware/uploadMiddleware");
 
 const router = express.Router();
 
 router.post("/", authMiddleware, upload.single("image"), imageCompressor, createProduct);
 router.get("/", authMiddleware, getAllProducts);
 router.get("/:productId", authMiddleware, getProductById);
-router.put("/:id", authMiddleware, upload.single("image"), updateProduct);
+router.put("/:id", authMiddleware, upload.single("image"), imageCompressor, updateProduct);
 router.delete("/:id", authMiddleware, deleteProduct);
 
 module.exports = router;
